5dc9a07f4d92b44e7441c5b698ed449c0a823147
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2024-09-21 17:54:19 +0530

    create main app

--- identiv/urls.py
+++ identiv/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from django.conf.urls.static import static
 from . import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("app/", include("app.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
