87ebb332a044b6dacbfaaca94b7282926457bb49
Avaneesh-Chopdekar <avaneeshchopdekar42@gmail.com>
2024-09-23 17:22:49 +0530

    reset migrations for adding uid (uuid v4) as primary key

--- app/models.py
+++ app/models.py
@@ -2,11 +2,13 @@ from django.contrib.auth.models import AbstractUser
 from django.contrib.postgres.fields import ArrayField
 from django.db import models
 from django.contrib.auth import get_user_model
+import uuid
 
 from app.manager import OrganizationManager
 
 
 class Organization(AbstractUser):
+    uid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(unique=True)  # Make email the unique identifier
     name = models.CharField(max_length=255, blank=True, null=True)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
@@ -27,6 +29,7 @@ User = get_user_model()
 
 
 class Person(models.Model):
+    uid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     organization = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=150)
     face_embedding = ArrayField(models.FloatField(), size=128)
@@ -37,6 +40,7 @@ class Person(models.Model):
 
 
 class LoginLog(models.Model):
+    uid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     person = models.ForeignKey(Person, on_delete=models.CASCADE)
     login_time = models.DateTimeField(auto_now_add=True)
 
